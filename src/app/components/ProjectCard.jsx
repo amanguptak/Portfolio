91ac12cd59d547e612767d36db018f9c65410456
Aman Gupta <eagleax7@gmail.com>
2024-01-07 17:59:08 +0530

    imgfixed

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -6,8 +6,8 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
-        className="h-52  md:h-72 rounded-xl relative group border border-amber-400 "
-        style={{ background: `url(${imgUrl})`, backgroundSize:"inherit"}}
+        className="h-fit w-fit  md:h-72 rounded-xl relative group border border-amber-400 "
+        style={{ background: `url(${imgUrl})`, backgroundSize:"cover"}}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
